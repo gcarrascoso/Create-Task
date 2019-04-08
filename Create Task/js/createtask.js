2e11deb1de4711232e0c67164393886a7f6dc92f
gcarrascoso <43757553+gcarrascoso@users.noreply.github.com>
2019-04-07 23:04:17 -0400

    Update createtask.js

--- Create Task/js/createtask.js
+++ Create Task/js/createtask.js
@@ -8,7 +8,9 @@ var upgradeCost2 = 900;
 var upgradeCost3 = 10000;
 var upgradeCost4 = 100000;
 var upgradeCost5 = 500000000;
-var upgradeCost6 = 30;
+var upgradeCost6 = 50;
+var upgradeCost7 = 450;
+var upgradeCost8 = 2500;
 var start = false;
 var farmerAmt = 0;
 var amAmt = 0;
@@ -16,9 +18,13 @@ var bbAmt = 0;
 var emAmt = 0;
 var aiAmt = 0;
 var juiceMkrAmt = 0;
+var cbAmt = 0;
+var spAmt = 0;
 var scoreMltply = 1;
 var tjps=0;
-var timeScore = 0;
+var timeScore6 = 0;
+var timeScore7 = 0;
+var timeScore8 = 0;
 
 
 function clickButton() {
@@ -111,22 +117,65 @@ function upgradeFive() {
   }
 }
 
-function upgradeSix() {
+function upgradeSix () {
   if (clickScore >= upgradeCost6) {
     clickScore = clickScore - upgradeCost6;
     upgradeCost6 = upgradeCost6 * 3;
     juiceMkrAmt++;
+    timeScore6++;
     var p = document.getElementById("upgrade6");
     p.innerHTML = upgradeCost6 + " TJ";
     var g = document.getElementById("juiceMkrAmt");
     g.innerHTML = (juiceMkrAmt + " Juice Makers");
+    timeSix();
   }
-  time();
 }
-function time() {
-  timeScore = 1;
-  clickScore = (clickScore) + (timeScore);
+function timeSix() {
+  timeScore6 = 1;
+  clickScore = (clickScore) + (timeScore6);
   var q = document.getElementById("score");
   q.innerHTML = clickScore;
-  setTimeout(time, 500);
+  setTimeout(timeSix, 250);
+}
+
+function upgradeSeven () {
+  if (clickScore >= upgradeCost7) {
+    clickScore = clickScore - upgradeCost7;
+    upgradeCost7 = upgradeCost7 * 4;
+    cbAmt++;
+    timeScore7 = timeScore7 + 5;
+    var p = document.getElementById("upgrade7");
+    p.innerHTML = upgradeCost7 + " TJ";
+    var g = document.getElementById("cbAmt");
+    g.innerHTML = (cbAmt + " Conveyor Belts");
+    timeSeven();
+  }
+}
+function timeSeven() {
+  timeScore7 = 10;
+  clickScore = (clickScore) + (timeScore7);
+  var q = document.getElementById("score");
+  q.innerHTML = clickScore;
+  setTimeout(timeSeven, 150);
+}
+
+function upgradeEight () {
+  if (clickScore >= upgradeCost8) {
+    clickScore = clickScore - upgradeCost8;
+    upgradeCost8 = upgradeCost8 * 5;
+    spAmt++;
+    timeScore8 = timeScore8 + 10;
+    var p = document.getElementById("upgrade8");
+    p.innerHTML = upgradeCost8 + " TJ";
+    var g = document.getElementById("spAmt");
+    g.innerHTML = (spAmt + " Solar Panels");
+    timeEight();
+  }
+}
+function timeEight() {
+  timeScore8 = 10;
+  clickScore = (clickScore) + (timeScore8);
+  var q = document.getElementById("score");
+  q.innerHTML = clickScore;
+  setTimeout(timeEight, 150);
 }
